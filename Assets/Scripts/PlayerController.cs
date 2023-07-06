3e7ed5d1572b05a352b690baf81dfce108ba4213
LIB0RG <52547190+LIB0RG@users.noreply.github.com>
2023-07-06 22:09:46 +0300

    lesson 12

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D myRB;
     private Animator myAnim;
